5afc0e936b797c4a76529eeb538e80659be28ffc
sarojbomjan <sarojbomjan7@gmail.com>
2024-10-18 11:36:40 +0545

    map marker

--- week1/lib/pages/mapclass.dart
+++ week1/lib/pages/mapclass.dart
@@ -1,5 +1,6 @@
 import "dart:async";
 import "package:flutter/material.dart";
+import "package:flutter/services.dart";
 import "package:google_maps_flutter/google_maps_flutter.dart";
 
 class MapSample extends StatefulWidget {
@@ -19,26 +20,58 @@ class MapSampleState extends State<MapSample> {
   );
 
   static const CameraPosition _kLake = CameraPosition(
-      bearing: 192.8334901395799,
-      target: LatLng(37.43296265331129, -122.08832357078792),
-      tilt: 59.440717697143555,
+      // bearing: 192.8334901395799,
+      target: LatLng(27.70415, 85.30647),
+      // tilt: 59.440717697143555,
       zoom: 19.151926040649414);
+
+  var _maptheme;
+
+  Future _loadMapTheme() async {
+    _maptheme = await rootBundle.loadString('raw/maptheme.json');
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _loadMapTheme();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: GoogleMap(
-        mapType: MapType.hybrid,
+        style: _maptheme,
+        mapType: MapType.normal,
         initialCameraPosition: _kGooglePlex,
         onMapCreated: (GoogleMapController controller) {
           _controller.complete(controller);
+
+          // controller.(_maptheme);
+        },
+        markers: {
+          Marker(
+            markerId: MarkerId("Bookverse"),
+            position: LatLng(27.6734, 85.3250),
+            infoWindow: InfoWindow(title: "Bookverse"),
+            onTap: () async {
+              final GoogleMapController controller = await _controller.future;
+              controller.animateCamera(
+                CameraUpdate.newLatLngZoom(LatLng(27.6992, 85.3127), 18),
+              );
+            },
+          ),
         },
       ),
-      floatingActionButton: FloatingActionButton.extended(
-        onPressed: _goToTheLake,
-        label: const Text('To the lake!'),
-        icon: const Icon(Icons.directions_boat),
+      floatingActionButton: Container(
+        margin: EdgeInsets.only(bottom: 20),
+        child: FloatingActionButton.extended(
+          onPressed: _goToTheLake,
+          label: const Text('To Kathmandu'),
+          icon: const Icon(Icons.home),
+        ),
       ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.miniStartFloat,
     );
   }
 
